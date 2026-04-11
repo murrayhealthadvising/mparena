611e57663eaf099357b5ca716dfd88ee19d1d97f
murrayhealthadvising <murrayhealthadvising@gmail.com>
2026-04-11 15:29:15 -0400

    Fix: add openid+email scopes so userinfo endpoint works

--- api/gmail-auth.js
+++ api/gmail-auth.js
@@ -1,1 +1,18 @@
-const{google}=require('googleapis');const REDIRECT_URI='https://mparena.vercel.app/api/gmail-callback';module.exports=async(req,res)=>{const oauth2Client=new google.auth.OAuth2(process.env.GOOGLE_OAUTH_CLIENT_ID,process.env.GOOGLE_OAUTH_CLIENT_SECRET,REDIRECT_URI);const authUrl=oauth2Client.generateAuthUrl({access_type:'offline',scope:['https://www.googleapis.com/auth/gmail.readonly','https://www.googleapis.com/auth/gmail.modify'],prompt:'consent',state:req.query.userId||''});res.redirect(authUrl);};
+const{google}=require('googleapis');
+const REDIRECT_URI='https://mparena.vercel.app/api/gmail-callback';
+module.exports=async(req,res)=>{
+  const oauth2Client=new google.auth.OAuth2(process.env.GOOGLE_OAUTH_CLIENT_ID,process.env.GOOGLE_OAUTH_CLIENT_SECRET,REDIRECT_URI);
+  const authUrl=oauth2Client.generateAuthUrl({
+    access_type:'offline',
+    scope:[
+      'openid',
+      'email',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/gmail.readonly',
+      'https://www.googleapis.com/auth/gmail.modify',
+    ],
+    prompt:'consent',
+    state:req.query.userId||''
+  });
+  res.redirect(authUrl);
+};
